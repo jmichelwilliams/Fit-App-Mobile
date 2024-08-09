6be97ad76914bcd1ee1afb5f520fcd89afe6fb8f
James Michel-Williams <jmichelwilliams@gmail.com>
2024-08-09 16:27:54 -0400

    Created Stack

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ProgramScreen from './screens/ProgramScreen';
+import ProgramDetailsScreen from './screens/ProgramDetailsScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Fit App</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Program" component={ProgramScreen} />
+          <Stack.Screen
+            name="ProgramDetails"
+            component={ProgramDetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
